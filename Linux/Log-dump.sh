fb80a84f2cad32c2de6ec58ca5874cce3ddf8789
LeeXDA18 <43325188+LeeXDA21@users.noreply.github.com>
2022-01-17 13:19:10 +0000

    Create Log-dump.sh
    Add support to grab prev_dump.log using Linux.

---
+++ Linux/Log-dump.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "${0%/*}"
+echo
+echo Detecting devices...
+echo
+./adb devices
+echo Saving prev_dump.log...
+echo
+./adb shell cat /data/log/prev_dump.log > log_dump.log
+echo prev_dump.log saved!
+echo
